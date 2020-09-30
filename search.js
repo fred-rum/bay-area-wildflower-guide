f8edc798f2bfc3d14fb5131e350cd38f7de4d40e
fred-rum <fred-rum@users.noreply.github.com>
2020-09-29 21:57:52 -0700

    test

--- search.js
+++ search.js
@@ -498,3 +498,18 @@ function oninteractive() {
   setTimeout(restore_scroll, 0);
 }
 oninteractive();
+
+
+/*****************************************************************************/
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
