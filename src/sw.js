2bff365261403e0a737db8a923de3f1dfde3a654
fred-rum <fred-rum@users.noreply.github.com>
2020-10-03 07:06:16 -0700

    Cleaned up race conditions and added tons of comments
    Also added code to compare the old url_data vs. new_url_data, in case that's the only update needed.

--- src/sw.js
+++ src/sw.js
@@ -2,19 +2,115 @@ var url_to_base64 = [
 /* insert code here */
 ];
 
+console.info('starting from the beginning');
+
 var DB_NAME = 'db-v1';
 var DB_VERSION = 1;
 var BASE64_CACHE_NAME = 'base64-cache-v1';
 
+// An initial 'updating' message prevents updates until everything
+// is initialized.
 var updating = 'Checking cache';
 var err_status = '';
-var kb_total = 0;
-var kb_cached = 0;
-var new_url_data = {};
-var base64_to_kb = {};
-var base64_to_delete = [];
 var usage = '';
 
+var url_data = undefined;
+var new_url_data;
+var url_diff;
+
+var base64_to_kb = {};
+var base64_to_delete = [];
+
+var kb_total = 0;
+var kb_cached = 0;
+
+
+/*** Install the service worker ***/
+
+// If this service worker (sw.js) is already registered, then the
+// browser has it in its cache and executes it before fetching
+// *anything* from the site it is registered to handle.  In this
+// scenario, the service handler registers its fetch handler and
+// handles the site fetches either from cache or online.  Eventually
+// we fetch swi.js, and when that executes, it tries to register
+// sw.js again.  If sw.js can't be found online, then the new
+// registration process is tossed out, and we just keep executing
+// with the browser's copy of sw.js.  The same thing happens if the
+// online sw.js is identical to the browser's copy.  However, if
+// the online sw.js is different, then it kicks off the installation
+// process.
+//
+// Now in a normal PWA, the 'install' handler goes off and fetches
+// and caches all the URLs that it wants.  Then, when the service
+// worker gets activated (e.g. during the next site visit, perhaps
+// while offline), it can fetch the cached URLs instead of downloading
+// them online.
+//
+// However, my PWA is different and does essentially nothing when it
+// is installed.  Only when there is explicit user interaction will
+// it start updating the cache.
+//
+//
+// The above describes what happens when the service worker is already
+// registered.  If it is being encountered for the first time, then
+// nothing happens until swi.js tries to register it.  The browser
+// fetches sw.js and executes it, then calls the 'install' handler.
+// In this case, the service worker doesn't process any fetches
+// until it is activated.
+
+self.addEventListener('install', fn_install);
+
+async function fn_install(event) {
+  console.info('fn_install()');
+
+  // If there was a previous service worker, by default this new one
+  // simply waits during this session and becomes the new service worker
+  // in the *next* session.  By calling skipWaiting(), we immediately
+  // replace the previous service worker to become the active one.
+  //
+  // We don't care if skipWaiting completes while we're in this handler
+  // or if it happens later in the waiting state, so we don't need to
+  // wrap it in event.waitUtil().
+  self.skipWaiting();
+}
+
+
+/*** when the service worker is activated ***/
+
+// Once installation of a service worker is finished, it is activated.
+// But it won't do anything until the next refresh.  To make it start
+// processing fetch requests immediately, we call claim().  We call it
+// for all clients, not just the one that registered the service worker,
+// because we want all windows to stay in sync.
+//
+// Note that it's not just that all clients are running the same version
+// of the service worker.  All clients are *sharing* the same service worker,
+// so we generally don't to worry about changes happening elsewhere and not
+// being reflected here.
+//
+// When we claim the service worker, the browser lets the previous one
+// finish what it's doing before killing it.  If it's just fetching
+// something from the cache, the transactions are atomic and so safe.
+// But if the other worker is in the middle of an update, and then the new
+// one also starts updating, weird things could happen.  That requires the
+//  following sequence:
+// - the user starts an update
+// - I update sw.js on the server
+// - the user hits refresh (or navigates around before returning to index.html)
+// - the user starts another update
+// This last step isn't ridiculous since as far as the user can tell, the
+// previous update just mysteriously stopped.
+//
+// Even here, the results shouldn't be terrible.  The kb counts could go
+// weird, but all the right files should end up in the indexedDB and cache.
+
+self.addEventListener('activate', event => {
+  console.info('activate');
+  clients.claim();
+});
+
+
+/*** convert callbacks to promises ***/
 
 // Some interfaces use callbacks, but we'd rather use Promises.
 // async_callbacks() converts a callback interface into a Promise.
@@ -59,137 +155,99 @@ function async_tx(tx) {
 }
 
 
-// Figure out the total size of the data to be cached.
-for (let i = 0; i < url_to_base64.length; i++) {
-  kb_total += url_to_base64[i][2];
-}
+/*** Handle a fetch request ***/
 
-console.info('scope = ' + registration.scope);
+// The 'fetch' event handler must be added synchronously during the
+// first script execution.  We cannot wait until the URL DB has been
+// read, because that's asynchronous!  See the fetch_handler() code
+// below for how we handle the race of read_db() and fetch_handler().
 
+console.info('adding fetch handler');
+self.addEventListener('fetch', fetch_handler);
 
-
-
-// Check how many KB of the new base64 values already have a file cached.
-async function count_cached() {
-  new_url_data = {};
-  for (let i = 0; i < url_to_base64.length; i++) {
-    let url = url_to_base64[i][0];
-    let base64 = url_to_base64[i][1];
-    let kb = url_to_base64[i][2];
-    new_url_data[url] = {base64: base64,
-                         kb: kb};
-    base64_to_kb[base64] = kb;
-  }
-
-  cache = await caches.open(BASE64_CACHE_NAME);
-  requests = await cache.keys();
-  console.info('checking cache keys');
-  for (let i = 0; i < requests.length; i++) {
-    let base64 = remove_scope_from_request(requests[i]);
-    if (base64 in base64_to_kb) {
-      kb_cached += base64_to_kb[base64];
-
-      // Entries left in base64_to_kb are ones that need to be fetched.
-      delete base64_to_kb[base64];
-    } else {
-      // Entries in base64_to_delete are ones that need to be deleted.
-      console.info('Need to delete ' + base64);
-      base64_to_delete.push(base64);
-    }
-  }
-
-  is_update_done();
-}
-count_cached();
-
-// Get the URL from the request and remove the prepended scope.
-// This can be done with a fetch request (which has a real URL)
-// or on a cache key (which has a base64 value in place of the
+// Remove the prepended scope from the URL of a request.
+// The scope is a string such as 'https://localhost:8000/'.
+//
+// This function can be performed on a real fetch request (which has
+// a real URL) or on a key from the cache (which the cache has tried
+// to format as a URL, but with a base64 value in place of the
 // relative URL).
 function remove_scope_from_request(request) {
   return request.url.substr(registration.scope.length);
 }
 
-/*** Install the service worker ***/
+// Handle a fetch request from a client (window/tab).
+// Map the URL to a base64 encoding (from the indexedDB), then
+// map the base64 encoding to a cache entry.
 
-self.addEventListener('install', fn_install);
+function fetch_handler(event) {
+  // The request is guaranteed to be in scope, so we can simply
+  // remove the scope to get the relative URL.
+  let url = remove_scope_from_request(event.request);
+  console.info('fetching ' + url);
 
-async function fn_install(event) {
-  // Since the install function requires asynchronous communication to
-  // complete, we use event.waitUntil() to keep the process alive until
-  // it completes.
-  event.waitUntil(fn_install2());
+  if ((!url_data) || (url in url_data)) {
+    // There is a race condition between initializing url_data
+    // and fetching the first URL.  Fortunately, our fetch
+    // response is allowed to be asynchronous.  So if we recognize
+    // the URL *or* we don't have url_data yet, tell the event
+    // to wait until we can create a fetch response.
+    event.respondWith(fetch_response(event, url));
+  } else {
+    // I found some documentation that says that performance is
+    // better if we simply fail to handle a request when making
+    // the default online fetch is fine.  So that's what we do
+    // if we have url_data and it's an unrecognized URL.  In
+    // most cases this will be because the user never pressed
+    // the shiny green button to cache anything.
+    console.info(url + ' not recognized')
+    return;
+  }
 }
 
-async function fn_install2() {
-  // Immediately replace any previous service worker.
-  await self.skipWaiting();
+async function fetch_response(event, url) {
+  // As described in fetch_handler() above, we might be here because
+  // we don't have url_data yet.  Presumably url_data is already in
+  // the process of being read from the indexedDB, but I'm not sure
+  // how to wait for that result.  Instead I take the simple path of
+  // simply reading it again.
+  if (!url_data) {
+    await read_db();
+  }
 
-  // Remove any old caches I might have left lying around from
-  // previous versions of the code.
-  var cacheNames = await caches.keys();
-  await Promise.all(
-    cacheNames.map(function(cacheName) {
-      if (cacheName !== BASE64_CACHE_NAME) {
-        return caches.delete(cacheName);
-      }
-    })
-  );
+  // Now we're guaranteed to have url_data and can proceed with
+  // checking the cache.
+
+  var response = await caches.match(url_data[url].base64);
+  console.info(response);
+  if (response) {
+    console.info(url + ' found')
+    return response;
+  } else {
+    console.info(url + ' not found')
+    return fetch(event.request);
+  }
 }
-
-
-/*** when the service worker is activated ***/
-
-// Once installation of a service worker is finished, it is activated.
-// But it won't do anything until the next refresh.  To make it start
-// processing fetch requests immediately, we call claim().  We call it
-// for all clients, not just the one that registered the service worker,
-// because we want all windows to stay in sync.
-//
-// Note that it's not just that all clients are running the same version
-// of the service worker.  All clients are *sharing* the same service worker,
-// so we generally don't to worry about changes happening elsewhere and not
-// being reflected here.
-//
-// When we claim the service worker, the browser lets the previous one
-// finish what it's doing before killing it.  If it's just fetching
-// something from the cache, the transactions are atomic and so safe.
-// But if the other worker is in the middle of an update, and then the new
-// one also starts updating.  That requires the following sequence:
-// - the user starts an update
-// - I update sw.js on the server
-// - the user hits refresh (or navigates around before returning to index.html)
-// - the user starts another update
-// This last step isn't ridiculous since as far as the user can tell, the
-// previous update just mysteriously stopped.
-//
-// Even here, the results shouldn't be terrible.  The kb counts could go
-// weird, but all the right files should end up in the indexedDB and cache.
-
-self.addEventListener('activate', event => {
-  clients.claim();
-});
 
 
 /*** Read the old url_data ***/
 
-// Note that this process is asynchronous, so we might get some requests
-// before it's done.  That may cause some files to get fetched online instead
-// of from the cache.  TODO: I should probably do this as part of registration
-// to be safe when offline.  I could borrow await_tx() to make it fit better
-// with promises.
-
-var db;
-var url_data = {};
-
-open_db();
+// Until the user manually updates the cache again, we want to continue
+// using the old url_data that was used to update the cache last time.
+// E>g.
+// - the user is online and updates the cache.
+// - the user is online and visits the site again and gets a new sw.js,
+//   but the user doesn't update the cache.
+// - the user goes offline.
+// In this case, the registered sw.js has a different URL->base64 map
+// than what was used when creating the cache.  Trying to use the new
+// url_data would result in cache misses, which is bad.
+//
+// The indexedDB stores the url_data value that was present when the
+// cache was last updated.  We use this old url_data until the user
+// updates the cache again.
 
 async function open_db() {
-  if (db) {
-    // The indexedDB is already open.
-    return
-  }
-
   let request = indexedDB.open(DB_NAME, DB_VERSION);
 
   // There is no documented provision for the request to wait for the
@@ -206,15 +264,31 @@ async function open_db() {
   // necessary.
   request.onupgradeneeded = dbupgradeneeded;
 
-  db = await async_callbacks(request, dbupgradeneeded);
+  db = await async_callbacks(request);
 
-  let lookup = await db.transaction("url_data").objectStore("url_data").get("key");
+  console.info('open_db() returns ' + db);
+  return db;
+}
 
-  console.info('get_key returned ' + lookup);
+// Read the cached url_data.
+async function read_db() {
+  console.info('read_db()');
+
+  let db = await open_db();
+
+  let tx = db.transaction("url_data").objectStore("url_data").get("key");
+  let lookup = await async_callbacks(tx);
+
+  console.info('lookup = ');
+  console.info(lookup);
   if (lookup){
     url_data = lookup.value;
+    console.info('url_data = ')
     console.info(url_data);
   }
+
+  // Note that we throw away the db value when we exit.
+  // My guess is that this allows the DB connection to close.
 }
 
 // This function is called when the indexedDB is accessed for the first time.
@@ -238,35 +312,116 @@ async function dbupgradeneeded(event) {
 }
 
 
-/*** Handle a fetch request ***/
+/*** Get the cache status ***/
 
-// Handle a fetch request from a client (window/tab).
-// Map the URL to a base64 encoding (from the indexedDB), then
-// map the base64 encoding to a cache entry.
-self.addEventListener('fetch', function(event) {
-  // The request is guaranteed to be in scope, so we can simply
-  // remove the scope to get the relative URL.
-  let url = remove_scope_from_request(event.request);
-  console.info('fetching ' + url);
-  if (url_data && (url in url_data)) {
-    event.respondWith(fetch_response(event, url));
-  } else {
-    // Allow the default fetch response.
-    console.info(url + ' not recognized')
-    return;
-  }
-});
+// This code figure out how much information has been cached and how much
+// more would be cached if the user asks for a cache update.
 
-async function fetch_response(event, url) {
-  var response = await caches.match(url_data[url].base64);
-  console.info(response);
-  if (response) {
-    console.info(url + ' found')
-    return response;
-  } else {
-    console.info(url + ' not found')
-    return fetch(event.request);
+init_status();
+
+async function init_status() {
+  console.info('init_status()');
+
+  // Do two things in parallel:
+  //
+  // 1. Read the old url_data from indexedDB.
+  // Note that the fetch handler will also call read_db() if it needs it
+  // before we're done here.  But we still call it here in order to
+  // compare it to the new_url_data.
+  //
+  // 2. Generate new_url_data and check how much of it is already cached.
+  await Promise.all([read_db(),
+                     count_cached()]);
+
+  // Compare the old url_data with new_url_data.
+  // This tells us whether we need to update the indexedDB.
+  // Generally we'd expect to only need to update the indexedDB when
+  // there are also cache changes to perform, but there are other
+  // possibilities, e.g.
+  // - I swap the names (URLs) of two files in the cache, or
+  // - The user manually deletes indexedDB.
+  console.info('compare url_data to new_url_data');
+  url_diff = false;
+
+  // Check if any old URLs are missing from new_url_data or have
+  // different base64 values.
+  for (let url in url_data) {
+    if (!(url in new_url_data) || (url_data[url] != new_url_data[url])) {
+      url_diff = true;
+    }
   }
+
+  // Check if any new URLs are missing from the old url_data.
+  // We already compared the base64 values for entries that match,
+  // so we don't have to do that part again.
+  for (let url in new_url_data) {
+    if (!(url in url_data)) {
+      url_diff = true;
+    }
+  }
+
+  // Finally, prepare the appropriate info to send to swi.js.
+  is_cache_up_to_date();
+}
+
+// Check how many KB of the new base64 values already have a file cached.
+// Also compute the total KB that will be cached if the cache is updated.
+async function count_cached() {
+  console.info('count_cached()');
+
+  new_url_data = {};
+  for (let i = 0; i < url_to_base64.length; i++) {
+    let url = url_to_base64[i][0];
+    let base64 = url_to_base64[i][1];
+    let kb = url_to_base64[i][2];
+    new_url_data[url] = {base64: base64,
+                         kb: kb};
+    base64_to_kb[base64] = kb;
+    kb_total += kb;
+  }
+
+  cache = await caches.open(BASE64_CACHE_NAME);
+  requests = await cache.keys();
+  console.info('checking base64 keys in the cache');
+  for (let i = 0; i < requests.length; i++) {
+    let base64 = remove_scope_from_request(requests[i]);
+    if (base64 in base64_to_kb) {
+      kb_cached += base64_to_kb[base64];
+
+      // Entries left in base64_to_kb are ones that need to be fetched.
+      delete base64_to_kb[base64];
+    } else {
+      // Entries in base64_to_delete are ones that need to be deleted.
+      console.info('Need to delete ' + base64);
+      base64_to_delete.push(base64);
+    }
+  }
+}
+
+
+/*** delete unused caches ***/
+
+// This code is not needed by any other code, so it happens whenever
+// it feels like and finishes whenever it wants.
+
+delete_unused_caches();
+
+async function delete_unused_caches() {
+  console.info('delete_unused_caches()');
+
+  // Remove any old caches I might have left lying around from
+  // previous versions of the code.
+  var cacheNames = await caches.keys();
+
+  // I deliberately skip an 'await' on this Promise because I don't need
+  // the results for anything.
+  Promise.all(
+    cacheNames.map(function(cacheName) {
+      if (cacheName !== BASE64_CACHE_NAME) {
+        return caches.delete(cacheName);
+      }
+    })
+  );
 }
 
 
@@ -311,6 +466,9 @@ function fn_send_status(event) {
   } else if (event.data === 'clear') {
     clear_caches(event);
   }
+}
+
+function clear_caches(event) {
 }
 
 // When requested, switch to using the newest url_data
@@ -369,7 +527,7 @@ async function update_cache(event) {
     }
   }
 
-  is_update_done();
+  is_cache_up_to_date();
 }
 
 async function record_urls() {
@@ -380,8 +538,8 @@ async function record_urls() {
   await async_callbacks(db.transaction("url_data", "readwrite").objectStore("url_data").put(obj));
 }
 
-function is_update_done() {
-  if (base64_to_delete.length || Object.keys(base64_to_kb).length) {
+function is_cache_up_to_date() {
+  if (base64_to_delete.length || Object.keys(base64_to_kb).length || url_diff) {
     updating = false;
   } else {
     updating = 'Up to date';
