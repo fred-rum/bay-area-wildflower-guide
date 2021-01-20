36ac6e65b4854f3dd0b5e1d9fd9e3aad03bcf4ec
fred-rum <fred-rum@users.noreply.github.com>
2021-01-19 22:54:26 -0800

    Added bugguide links to remaining insects (moths and galling insects)

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
-var upd_timestamp = '2021-01-20T03:36:55.150369+00:00';
+var upd_timestamp = '2021-01-20T06:53:33.526198+00:00';
 var upd_num_urls = 5308;
-var upd_kb_total = 690900
+var upd_kb_total = 690901
 console.info('starting from the beginning');
 const DB_NAME = 'db-v1';
 const DB_VERSION = 1;
