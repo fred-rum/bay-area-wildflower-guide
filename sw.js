e324be773f7e9a8db326c874a089431ea31214ea
fred-rum <fred-rum@users.noreply.github.com>
2021-05-26 19:50:54 -0700

    Various content updates

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
-var upd_timestamp = '2021-03-17T02:31:04.204954+00:00';
-var upd_num_urls = 5882;
-var upd_kb_total = 731248
+var upd_timestamp = '2021-05-26T05:46:30.960675+00:00';
+var upd_num_urls = 5923;
+var upd_kb_total = 734350
 const DB_NAME = 'db-v1';
 const DB_VERSION = 1;
 const BASE64_CACHE_NAME = 'base64-cache-v1';
