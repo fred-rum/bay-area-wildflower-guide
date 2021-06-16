35776c3a0bbbafba8a5ea6132a2412fd60104a7a
fred-rum <fred-rum@users.noreply.github.com>
2021-06-15 19:21:22 -0700

    Add Vancouveria

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
-var upd_timestamp = '2021-06-13T14:44:01.364913+00:00';
-var upd_num_urls = 6005;
-var upd_kb_total = 745705
+var upd_timestamp = '2021-06-16T02:16:28.225034+00:00';
+var upd_num_urls = 6017;
+var upd_kb_total = 746957
 const DB_NAME = 'db-v1';
 const DB_VERSION = 1;
 const BASE64_CACHE_NAME = 'base64-cache-v1';
