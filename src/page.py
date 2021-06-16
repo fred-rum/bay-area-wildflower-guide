c6832a4f4099fc5a104e66eccfae89b375ab113f
fred-rum <fred-rum@users.noreply.github.com>
2021-06-15 19:22:50 -0700

    Remove support for 'complete [txt]' property and update documentation
    'complete [txt]' was implicitly deprecated by a previous release that improved the handling of 'x:', but now I'm finally removing the property.
    
    That 'x:' improvement also wasn't documented yet, so I fixed that as well.

--- src/page.py
+++ src/page.py
@@ -845,7 +845,7 @@ class Page:
         self.txt = re.sub(r'^default_ancestor\s*?\n',
                           repl_default_ancestor, self.txt, flags=re.MULTILINE)
 
-        self.txt = re.sub(r'^(create|link|member_link|member_name|photo_requires_color|color_requires_photo|obs_requires_photo|flag_one_child|allow_obs_promotion|flag_obs_promotion|flag_obs_promotion_above_peers|flag_obs_promotion_without_x|allow_casual_obs|allow_outside_obs|allow_outside_obs_promotion|obs_fill_com|obs_fill_sci|obs_fill_alt_com|flag_obs_fill_com|flag_obs_fill_sci|flag_obs_fill_alt_com|link_calflora|link_calphotos|link_jepson|link_birds|link_bayarea_calflora|link_bayarea_inaturalist|complete\s*(?:none|ba|ca|any|hist|rare|hist/rare|more|uncat)):\s*(.*?)\s*?\n',
+        self.txt = re.sub(r'^(create|link|member_link|member_name|photo_requires_color|color_requires_photo|obs_requires_photo|flag_one_child|allow_obs_promotion|flag_obs_promotion|flag_obs_promotion_above_peers|flag_obs_promotion_without_x|allow_casual_obs|allow_outside_obs|allow_outside_obs_promotion|obs_fill_com|obs_fill_sci|obs_fill_alt_com|flag_obs_fill_com|flag_obs_fill_sci|flag_obs_fill_alt_com|link_calflora|link_calphotos|link_jepson|link_birds|link_bayarea_calflora|link_bayarea_inaturalist):\s*(.*?)\s*?\n',
                           repl_property, self.txt, flags=re.MULTILINE)
 
     def parse_glossary(self):
@@ -2083,7 +2083,7 @@ class Page:
         # No ancestor matched
         return False
 
-    # Find the loosest child constraint, then look to see if
+    # Find the loosest child completeness constraint, then look to see if
     # that matches any parent constraint.  If so, use it.
     #
     # Note that parent constraints ought to be at least as loose.  If
@@ -2222,24 +2222,15 @@ class Page:
 
             w.write(self.write_parents())
 
-            # Write the completeness of the taxon.  This could come from
-            # the 'complete' property or the 'x' or 'xx' flags.
-
-            if ('complete' in self.prop_value and
-                not self.genus_complete and
-                not self.species_complete):
-                if self.rank:
-                    rank_name = self.rank.name
-                else:
-                    rank_name = 'group'
-                write_complete(w,
-                               self.prop_value['complete'], False,
-                               True, rank_name);
+            # Write the completeness of the taxon (from the 'x' or 'xx' flags).
 
             is_top_of_genus = self.is_top_of(Rank.genus)
 
             complete = self.get_complete()
 
+            # We usually write something only if the completeness is explicit,
+            # but completeness is always expected for a genus, so we'll write
+            # a warning if it's missing.
             if complete or is_top_of_genus:
                 if is_top_of_genus:
                     rank_name = 'genus'
