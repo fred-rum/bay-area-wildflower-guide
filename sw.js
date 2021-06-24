d606782aa0bfae4836848828cc375fbe9089ecc7
fred-rum <fred-rum@users.noreply.github.com>
2021-06-23 22:57:12 -0700

    Added all the non-insect arthropods

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
-var upd_timestamp = '2021-06-17T22:53:59.663534+00:00';
-var upd_num_urls = 6017;
-var upd_kb_total = 746987
+var upd_timestamp = '2021-06-24T05:55:37.857684+00:00';
+var upd_num_urls = 6102;
+var upd_kb_total = 754150
 const DB_NAME = 'db-v1';
 const DB_VERSION = 1;
 const BASE64_CACHE_NAME = 'base64-cache-v1';
