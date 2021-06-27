99e6975683fb186a393d55a2b7de3884fcd9619a
fred-rum <fred-rum@users.noreply.github.com>
2021-06-26 21:08:49 -0700

    Added all observed beetles and a few related species

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
-var upd_timestamp = '2021-06-26T08:05:21.621031+00:00';
-var upd_num_urls = 6157;
-var upd_kb_total = 758381
+var upd_timestamp = '2021-06-27T03:55:52.492643+00:00';
+var upd_num_urls = 6282;
+var upd_kb_total = 766004
 const DB_NAME = 'db-v1';
 const DB_VERSION = 1;
 const BASE64_CACHE_NAME = 'base64-cache-v1';
