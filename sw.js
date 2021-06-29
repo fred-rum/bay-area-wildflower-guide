4ba99209cbdac98e7cad0dd00f6eea87f1cdd981
fred-rum <fred-rum@users.noreply.github.com>
2021-06-28 17:38:34 -0700

    Fixed cicadas

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
-var upd_timestamp = '2021-06-28T19:19:25.312262+00:00';
+var upd_timestamp = '2021-06-29T00:38:08.207956+00:00';
 var upd_num_urls = 6368;
-var upd_kb_total = 775073
+var upd_kb_total = 775072
 const DB_NAME = 'db-v1';
 const DB_VERSION = 1;
 const BASE64_CACHE_NAME = 'base64-cache-v1';
