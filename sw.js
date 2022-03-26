4c2241e158c1cfa856fb5af9bb50a1b7a7995111
fred-rum <fred-rum@users.noreply.github.com>
2022-03-26 00:02:35 -0700

    various database updates

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
-var upd_timestamp = '2022-03-18T01:49:37.266471+00:00';
-var upd_num_urls = 7004;
-var upd_kb_total = 749964
+var upd_timestamp = '2022-03-24T01:51:27.782893+00:00';
+var upd_num_urls = 7018;
+var upd_kb_total = 750803
 const DB_NAME = 'db-v1';
 const DB_VERSION = 1;
 const BASE64_CACHE_NAME = 'base64-cache-v1';
