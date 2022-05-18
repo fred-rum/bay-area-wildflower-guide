f241c8b12b21245340c7758f1ff243f9448fc1ca
fred-rum <fred-rum@users.noreply.github.com>
2022-05-18 16:47:14 -0700

    updated info

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
-var upd_timestamp = '2022-05-10T04:07:03.780565+00:00';
-var upd_num_urls = 7104;
-var upd_kb_total = 759179
+var upd_timestamp = '2022-05-18T23:47:05.762017+00:00';
+var upd_num_urls = 7186;
+var upd_kb_total = 767995
 const DB_NAME = 'db-v1';
 const DB_VERSION = 1;
 const BASE64_CACHE_NAME = 'base64-cache-v1';
