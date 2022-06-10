ec60622e6483dc160f37f940032c0c57ff260eaa
fred-rum <fred-rum@users.noreply.github.com>
2022-06-09 19:51:28 -0700

    updated info
    Galls are moved under "life" in order to include gall fungi.

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
-var upd_timestamp = '2022-06-01T18:06:55.607372+00:00';
-var upd_num_urls = 7240;
-var upd_kb_total = 775706
+var upd_timestamp = '2022-06-10T02:48:18.157146+00:00';
+var upd_num_urls = 7285;
+var upd_kb_total = 779564
 const DB_NAME = 'db-v1';
 const DB_VERSION = 1;
 const BASE64_CACHE_NAME = 'base64-cache-v1';
