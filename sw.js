2a817616e56fecec0bde8efc099a442784206e3e
fred-rum <fred-rum@users.noreply.github.com>
2022-07-12 19:10:47 -0700

    updated info
    lots of re-IDs and therefore photo updates for the white nightshades

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
-var upd_timestamp = '2022-07-12T19:05:42.942355+00:00';
-var upd_num_urls = 7738;
-var upd_kb_total = 839737
+var upd_timestamp = '2022-07-13T02:09:53.775059+00:00';
+var upd_num_urls = 7749;
+var upd_kb_total = 840833
 const DB_NAME = 'db-v1';
 const DB_VERSION = 1;
 const BASE64_CACHE_NAME = 'base64-cache-v1';
