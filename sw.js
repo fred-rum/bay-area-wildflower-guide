7c72121b927c77c0b650b111efd9f6922436ea69
fred-rum <fred-rum@users.noreply.github.com>
2022-08-09 16:25:08 -0700

    added some edibility info
    Also found a list of "toxic" plants from the California Poison Control System,
    but I haven't yet integrated it in any way.

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
-var upd_timestamp = '2022-08-09T21:39:33.891589+00:00';
+var upd_timestamp = '2022-08-09T23:04:04.892633+00:00';
 var upd_num_urls = 7964;
-var upd_kb_total = 868824
+var upd_kb_total = 868823
 const DB_NAME = 'db-v1';
 const DB_VERSION = 1;
 const BASE64_CACHE_NAME = 'base64-cache-v1';
