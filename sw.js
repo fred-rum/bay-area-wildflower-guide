713d31b42dcc1d635e3fd48bb7f9f287b7d84f7d
fred-rum <fred-rum@users.noreply.github.com>
2022-08-10 00:17:18 -0700

    more edibility info

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
-var upd_timestamp = '2022-08-10T04:23:11.191739+00:00';
+var upd_timestamp = '2022-08-10T07:14:32.282784+00:00';
 var upd_num_urls = 7964;
-var upd_kb_total = 868972
+var upd_kb_total = 868967
 const DB_NAME = 'db-v1';
 const DB_VERSION = 1;
 const BASE64_CACHE_NAME = 'base64-cache-v1';
