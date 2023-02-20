e1786c70a72ee3da22994ed82e38be5ec51b6574
fred-rum <fred-rum@users.noreply.github.com>
2023-02-20 00:18:18 -0800

    added random mustard notes

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
-var upd_timestamp = '2023-02-20T07:00:55.061532+00:00';
+var upd_timestamp = '2023-02-20T08:17:51.309566+00:00';
 var upd_num_urls = 8312;
-var upd_kb_total = 909565
+var upd_kb_total = 909580
 const DB_NAME = 'db-v1';
 const DB_VERSION = 1;
 const BASE64_CACHE_NAME = 'base64-cache-v1';
