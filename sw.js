758bb3117290823478daf8e4deb21c636d70d01e
fred-rum <fred-rum@users.noreply.github.com>
2023-03-02 17:08:47 -0800

    added more members with simple common names for flowering plants
    Also, a bit of toxicity clean up for Prunus (although it's still a mess).
    And removed all instances of "sci_P: n/a" in favor of aliases to n/a.

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
-var upd_timestamp = '2023-03-03T00:23:38.106949+00:00';
+var upd_timestamp = '2023-03-03T01:06:10.786954+00:00';
 var upd_num_urls = 8347;
-var upd_kb_total = 914186
+var upd_kb_total = 914193
 const DB_NAME = 'db-v1';
 const DB_VERSION = 1;
 const BASE64_CACHE_NAME = 'base64-cache-v1';
