f94181950a63bf0de784a68f89df07c3adfd5202
fred-rum <fred-rum@users.noreply.github.com>
2023-07-15 17:12:57 -0700

    removed some unnecessary CA's from the park names

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
-var upd_timestamp = '2023-07-15T08:16:41.109764+00:00';
+var upd_timestamp = '2023-07-16T00:12:19.792328+00:00';
 var upd_num_urls = 9005;
-var upd_kb_total = 1003884
+var upd_kb_total = 1003880
 const DB_NAME = 'db-v1';
 const DB_VERSION = 1;
 const BASE64_CACHE_NAME = 'base64-cache-v1';
