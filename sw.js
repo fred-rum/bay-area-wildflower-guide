f46fe7de80159608c5e21d23e50e09ce4f69af3a
fred-rum <fred-rum@users.noreply.github.com>
2023-08-28 10:00:03 -0700

    Fix references to src/ in stripped advanced-search.html and gallery.html

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 'use strict';
-var upd_timestamp = '2023-08-28T02:08:31.112859+00:00';
+var upd_timestamp = '2023-08-28T16:59:18.053582+00:00';
 var upd_num_urls = 9095;
 var upd_kb_total = 1016079
 const DB_NAME = 'db-v1';
