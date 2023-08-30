67d5e8809048249eeff132925b4e581565e070fb
fred-rum <fred-rum@users.noreply.github.com>
2023-08-30 15:50:23 -0700

    Fix parks.yaml for Monterey County, which was sucking up everything outside the bay area

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
-var upd_timestamp = '2023-08-30T22:49:08.624249+00:00';
+var upd_timestamp = '2023-08-30T22:49:51.288276+00:00';
 var upd_num_urls = 9095;
-var upd_kb_total = 1016088
+var upd_kb_total = 1016091
 const DB_NAME = 'db-v1';
 const DB_VERSION = 1;
 const BASE64_CACHE_NAME = 'base64-cache-v1';
