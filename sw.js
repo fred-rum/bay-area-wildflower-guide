1de934621dd0376ab28d6534aea346f84603697d
fred-rum <fred-rum@users.noreply.github.com>
2023-09-01 11:32:25 -0700

    added pages for each gall host genus
    and updated some other info

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
-var upd_timestamp = '2023-09-01T01:09:47.604526+00:00';
-var upd_num_urls = 9095;
-var upd_kb_total = 1016091
+var upd_timestamp = '2023-09-01T18:31:49.940907+00:00';
+var upd_num_urls = 9113;
+var upd_kb_total = 1016575
 const DB_NAME = 'db-v1';
 const DB_VERSION = 1;
 const BASE64_CACHE_NAME = 'base64-cache-v1';
