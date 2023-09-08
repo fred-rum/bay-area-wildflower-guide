071c9a12fa0891fd36410572f9ce157135290a20
fred-rum <fred-rum@users.noreply.github.com>
2023-09-07 18:19:06 -0700

    Rumex bucephalophorus is no longer thought to be in the bay area

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
-var upd_timestamp = '2023-09-07T23:45:07.517083+00:00';
+var upd_timestamp = '2023-09-08T01:18:40.909678+00:00';
 var upd_num_urls = 9257;
-var upd_kb_total = 1034978
+var upd_kb_total = 1034977
 const DB_NAME = 'db-v1';
 const DB_VERSION = 1;
 const BASE64_CACHE_NAME = 'base64-cache-v1';
