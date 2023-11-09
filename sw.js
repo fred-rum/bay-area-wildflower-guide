b825d76eeeb9482ef7360e6478fe8c4a4f05cac8
fred-rum <fred-rum@users.noreply.github.com>
2023-11-09 09:01:49 -0800

    reorganized galls
    Moved gall details from Linnaean hierarchy to non-Linnaean gall summary pages.
    Changed language from "parasitizes" or "parasitized by" to "forms on" or "hosts".  Add more detail on form of each gall.
    Fixed some minor info errors.

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
-var upd_timestamp = '2023-11-08T01:01:37.532974+00:00';
-var upd_num_urls = 9434;
-var upd_kb_total = 1059764
+var upd_timestamp = '2023-11-09T16:59:09.565308+00:00';
+var upd_num_urls = 9435;
+var upd_kb_total = 1059763
 const DB_NAME = 'db-v1';
 const DB_VERSION = 1;
 const BASE64_CACHE_NAME = 'base64-cache-v1';
