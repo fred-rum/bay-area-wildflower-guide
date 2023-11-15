225a913883b86a8d7fc150bedf60ee7be8676de3
fred-rum <fred-rum@users.noreply.github.com>
2023-11-14 21:00:12 -0800

    updated info

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
-var upd_timestamp = '2023-11-14T01:56:49.086062+00:00';
-var upd_num_urls = 9497;
-var upd_kb_total = 1067081
+var upd_timestamp = '2023-11-15T04:59:52.720672+00:00';
+var upd_num_urls = 9508;
+var upd_kb_total = 1067761
 const DB_NAME = 'db-v1';
 const DB_VERSION = 1;
 const BASE64_CACHE_NAME = 'base64-cache-v1';
