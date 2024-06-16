42d95d7d01d71256fa346eddf3774fae8e4dd53f
fred-rum <fred-rum@users.noreply.github.com>
2024-06-16 16:15:03 -0700

    fix Quicksilver park name

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
-var upd_timestamp = '2024-06-12T23:12:36.319989+00:00';
+var upd_timestamp = '2024-06-16T23:13:55.896620+00:00';
 var upd_num_urls = 10045;
-var upd_kb_total = 1141931
+var upd_kb_total = 1141938
 const DB_NAME = 'db-v1';
 const DB_VERSION = 1;
 const BASE64_CACHE_NAME = 'base64-cache-v1';
