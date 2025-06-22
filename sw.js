ab7ca48136c84a16c8d6c8beb3309071505e4566
fred-rum <fred-rum@users.noreply.github.com>
2025-06-22 08:52:19 -0700

    updated key for centauries

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
-var upd_timestamp = '2025-06-14T21:31:17.604456+00:00';
-var upd_num_urls = 10632;
-var upd_kb_total = 1223308
+var upd_timestamp = '2025-06-18T07:15:38.849702+00:00';
+var upd_num_urls = 10640;
+var upd_kb_total = 1224357
 const DB_NAME = 'db-v1';
 const DB_VERSION = 1;
 const BASE64_CACHE_NAME = 'base64-cache-v1';
